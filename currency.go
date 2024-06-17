6177c134b0dd42ec2dd8c0cb79fa24ff1ae4be7e
lishan <lsls907@163.com>
2024-06-17 16:50:06 +0800

    add currency

---
+++ currency.go
@@ -0,0 +1,13 @@
+package minutils
+
+import "math"
+
+// ToFixed 浮点数到指定小数位
+func ToFixed(num float64, precision int) float64 {
+	output := math.Pow(10, float64(precision))
+	return float64(round(num*output)) / output
+}
+
+func round(num float64) int {
+	return int(num + math.Copysign(0.5, num))
+}
